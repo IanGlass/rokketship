498e5290885842611cd9e12fa5c3f7dff8c3d9f7
IanGlass <37228869+IanGlass@users.noreply.github.com>
2021-05-10 07:50:37 +1200

    Add bullets and asteroid destruction

--- rokket_ship/game.py
+++ rokket_ship/game.py
@@ -13,9 +13,9 @@ class RokketShip:
         self.background = load_sprite("space", False)
         self.clock = pygame.time.Clock()
 
-        self.spaceship = Spaceship((400, 300))
-
+        self.bullets = []
         self.asteroids = []
+        self.spaceship = Spaceship((400, 300), self.bullets.append)
 
         for _ in range(6):
             while True:
@@ -28,7 +28,7 @@ class RokketShip:
         [Asteroid(get_random_position(self.screen)) for _ in range(6)]
 
     def _get_game_objects(self):
-        game_objects = [*self.asteroids]
+        game_objects = [*self.asteroids, *self.bullets]
 
         if self.spaceship:
             game_objects.append(self.spaceship)
@@ -51,6 +51,9 @@ class RokketShip:
             # Press ESC to quit game
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 quit()
+
+            elif (self.spaceship and event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE):
+                self.spaceship.shoot()
 
         is_key_pressed = pygame.key.get_pressed()
 
@@ -77,6 +80,19 @@ class RokketShip:
                 if asteroid.collides_with(self.spaceship):
                     self.spaceship = None
                     break
+
+        # Process bullet-asteroid collision
+        for bullet in self.bullets[:]:
+            for asteroid in self.asteroids[:]:
+                if asteroid.collides_with(bullet):
+                    self.asteroids.remove(asteroid)
+                    self.bullets.remove(bullet)
+                    break
+
+        # Remove bullets that are off screen
+        for bullet in self.bullets[:]:
+            if not self.screen.get_rect().collidepoint(bullet.position):
+                self.bullets.remove(bullet)
 
     # Updates display with any changes this tick
     def _draw(self):
--- rokket_ship/models.py
+++ rokket_ship/models.py
@@ -28,7 +28,9 @@ class Spaceship(GameObject):
     MANEUVERABILITY = 3
     # Determines how fast the ship can accelerate
     ACCELERATION = 0.25
-    def __init__(self, position):
+    BULLET_SPEED = 3
+    def __init__(self, position, create_bullet_callback):
+        self.create_bullet_callback = create_bullet_callback
         self.direction = Vector2(UP)
 
         super().__init__(position, load_sprite('spaceship'), Vector2(0))
@@ -49,6 +51,18 @@ class Spaceship(GameObject):
     def accelerate(self):
         self.velocity += self.direction * self.ACCELERATION
 
+    def shoot(self):
+        bullet_velocity = self.direction * self.BULLET_SPEED + self.velocity
+        bullet = Bullet(self.position, bullet_velocity)
+        self.create_bullet_callback(bullet)
+
 class Asteroid(GameObject):
     def __init__(self, position):
         super().__init__(position, load_sprite('asteroid'), get_random_velocity(1, 3))
+
+class Bullet(GameObject):
+    def __init__(self, position, velocity):
+        super().__init__(position, load_sprite('bullet'), velocity)
+
+    def move(self, surface):
+        self.position = self.position + self.velocity
